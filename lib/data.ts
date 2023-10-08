25accbc21c831b4635cf45398ce2756bfe6d58b7
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-10-08 11:43:18 +0100

    add data navbar

---
+++ lib/data.ts
@@ -0,0 +1,27 @@
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "News",
+    hash: "#news",
+  },
+  {
+    name: "Gallary",
+    hash: "#gallary",
+  },
+  {
+    name: "Vedio",
+    hash: "#vedio",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
